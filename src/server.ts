f1f51c1666ac2c19f17f4fd8f0ed2bd12c2e50ea
Akoruu.Dev <akoruu.dev@gmail.com>
2023-01-22 14:19:25 -0300

    FEAT [SERVER]: Connect server on database

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import races from "./routes/races.routes";
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 const server = express();
 server.use(express.json());
 server.use(cors);
+
+server.use(races)
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
